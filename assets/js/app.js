7a705c51d3a7241a630ca00a14102f060efffd36
javier <javierfdb@gmail.com>
2022-08-25 09:59:51 -0400

    estado inicial

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+/*--WOW--*/
+wow = new WOW(
+  {
+    animateClass: 'animated',
+    offset:       100,
+    mobile: false,
+    callback: function(box) {
+    }
+  }
+);
+wow.init();
+/*--FIN WOW--*/
